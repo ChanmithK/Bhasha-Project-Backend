c5eab8165ff0db865f8420548bee65ea9c9a15f9
Praveen Kariyawasam <107452850+PraveenTSG@users.noreply.github.com>
2024-08-02 12:34:59 +0530

    Added Proper comments

--- server.js
+++ server.js
@@ -7,21 +7,28 @@ const courseRoutes = require("./routes/courseRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
+
+// Connect to the database
 connectDB();
 
+// Create the Express app
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// Routes for students, courses, and authentication
 app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/courses", courseRoutes);
 
+// Default route
 app.use("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
